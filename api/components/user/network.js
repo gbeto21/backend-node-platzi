be41a9c941e0a463e6f2bd5403a00a8b4d614d62
Alberto González Balderas <albertoglezb@hotmail.com>
2020-01-19 21:19:47 +0100

    Se resolvió el reto para agregar un usuario. Se creó la documentación y se agregó el archivo swagger.json. Se terminó la clase: 'Documentación de nuestra API'.

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -3,7 +3,12 @@ const router = express.Router()
 const response = require('../../../network/response')
 const controller = require('./index')
 
-router.get('/', function (req, res) {
+router.get('/', list)
+router.get('/:id', get)
+router.post('/', upsert)
+router.put('/', upsert)
+
+function list(req, res) {
 
     controller.list()
         .then((lista) => {
@@ -13,9 +18,9 @@ router.get('/', function (req, res) {
             response.error(req, res, err.message, 500)
         })
 
-})
+}
 
-router.get('/:id', function (req, res) {
+function get(req, res) {
     controller.get(req.params.id)
         .then((user) => {
             response.success(req, res, user, 200)
@@ -23,18 +28,18 @@ router.get('/:id', function (req, res) {
         .catch((err) => {
             response.error(req, res, err.message, 500)
         })
-})
+}
 
-router.post('/', function (req, res) {
+function upsert(req, res) {
 
-    controller.upsert(req.body.data)
-        .then(() => {
-            response.success(req, res, null, 200)
+    controller.upsert(req.body)
+        .then((user) => {
+            response.success(req, res, user, 201)
         })
         .catch((err) => {
             response.error(req, res, err.message, 500)
         })
+}
 
-})
 
 module.exports = router
--- api/index.js
+++ api/index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const user = require('./components/user/network')
 const config = require('../config.js')
+const bodyParser = require('body-parser')
+const swaggerUi = require('swagger-ui-express')
+
+app.use(bodyParser.json())
+
+const swaggerDoc = require('./swagger.json')
 
 //Router
 app.use("/api/user", user)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 app.listen(config.api.port, () => {
     console.log('Api escuchando en el puerto ', config.api.port);
