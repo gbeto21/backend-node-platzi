2317152b8e87af4fefadeb8ad1ac8ece9e85a668
Alberto González Balderas <albertoglezb@hotmail.com>
2020-01-20 18:32:29 +0100

    Se agregó la funcionaldiad para revisar los permisos de funcionalidad. Se terminó la clase:Autenticación: gestión de permisos'.

--- api/auth/index.js
+++ api/auth/index.js
@@ -1,7 +1,43 @@
 const jwt = require('jsonwebtoken')
+const config = require('../../config')
+const secret = config.jwt.secret
 
 function sign(data) {
-    return jwt.sign(data, 'secreto')
+    return jwt.sign(data, secret)
+}
+
+const check = {
+    own: function (req, owner) {
+        const decoded = decodeHeader(req)
+        console.log(decoded)
+    }
+}
+
+function verify(token) {
+    return jwt.verify(token, secret)
+}
+
+function getToken(auth) {
+    if (!auth) {
+        throw new Error('No viene token')
+    }
+
+    if (auth.indexOf('Bearer') === -1) {
+        throw new Error('Formato inválido')
+    }
+
+    let token = auth.replace('Bearer', '')
+    return token
+
+}
+
+function decodeHeader(req) {
+    const authorization = req.headers.authorization || ''
+    const token = getToken(authorization)
+    const decoded = verify(token)
+
+    req.user = decoded
+    return decoded
 }
 
 module.exports = {
