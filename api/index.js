c2cab03d774ab208958604dac4418e15e7d54f6a
Alberto González Balderas <albertoglezb@hotmail.com>
2020-01-20 20:52:29 +0100

    Se aterminó la clase: 'Gestión avanzada de errores: Throw'

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const auth = require('./components/auth/network')
 const config = require('../config.js')
 const bodyParser = require('body-parser')
 const swaggerUi = require('swagger-ui-express')
+const errors = require('../network/errors')
 
 app.use(bodyParser.json())
 
@@ -14,6 +15,8 @@ const swaggerDoc = require('./swagger.json')
 app.use("/api/user", user)
 app.use("/api/auth", auth)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+
+// app.use(errors)
 
 app.listen(config.api.port, () => {
     console.log('Api escuchando en el puerto ', config.api.port);
--- auth/index.js
+++ auth/index.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const config = require('../config')
+const error = require('../utils/error')
 const secret = config.jwt.secret
 
 function sign(data) {
@@ -16,7 +17,7 @@ const check = {
         console.log(decoded)
 
         if (decoded.id !== owner) {
-            throw new Error('No puedes hacer esto')
+            throw error('No puedes hacer esto', 401)
         }
 
     }
